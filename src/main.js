5a06166ea2b0c2c95aef946bbb0208256d749858
Abhishek Anand <raianand@users.noreply.github.com>
2024-02-15 16:25:17 +0530

    added time logging to benchmark perf

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core')
-const { exec, getExecOutput } = require('@actions/exec')
+const { exec } = require('@actions/exec')
 const cache = require('@actions/cache');
 const { wait } = require('./wait')
 const { createInterceptDotPy } = require('./intercept')
@@ -13,6 +13,9 @@ const fs = require('fs')
  */
 async function run() {
   try {
+    let startTime = Date.now();
+    core.info(`Start time: ${startTime}`)
+    
     core.startGroup('create-bolt-user')
     core.info('Creating bolt user...')
     const boltUser = 'bolt'
@@ -21,6 +24,9 @@ async function run() {
     core.info('Creating bolt user... done')
     core.endGroup('create-bolt-user')
 
+    let endTime = Date.now();
+    core.info(`Time Elapsed in creating boltUser: ${Math.ceil((endTime - startTime)/1000)}s`)
+    startTime = endTime;
     
     core.startGroup('setup-bolt')
     
@@ -32,6 +38,10 @@ async function run() {
     //Verify that egress_rules_yaml is valid YAML
     YAML.parse(egress_rules_yaml)
     core.info("Reading inputs... done")
+
+    endTime = Date.now();
+    core.info(`Time Elapsed in reading inputs: ${Math.ceil((endTime - startTime)/1000)}s`)
+    startTime = endTime;
     
     core.info('Installing mitmproxy...')
 
@@ -55,9 +65,17 @@ async function run() {
     await exec(`sudo chown ${boltUser}:${boltUser} /home/${boltUser}/mitmdump`)
     core.info('Installing mitmproxy... done')
 
+    endTime = Date.now();
+    core.info(`Time Elapsed in installing MITMProxy: ${Math.ceil((endTime - startTime)/1000)}s`)
+    startTime = endTime;
+
     core.info('Create bolt output file...')
     await exec(`sudo -u ${boltUser} -H bash -c "touch /home/${boltUser}/output.log`)
     core.info('Create bolt output file... done')
+
+    endTime = Date.now();
+    core.info(`Time Elapsed in setting bolt output file: ${Math.ceil((endTime - startTime)/1000)}s`)
+    startTime = endTime;
 
     core.info('Create bolt config...')
     const boltConfig = `dump_destination: "/home/${boltUser}/output.log"`
@@ -67,17 +85,29 @@ async function run() {
     await exec(`sudo chown ${boltUser}:${boltUser} /home/${boltUser}/.mitmproxy/config.yaml`)
     core.info('Create bolt config... done')
 
+    endTime = Date.now();
+    core.info(`Time Elapsed in configuring bolt: ${Math.ceil((endTime - startTime)/1000)}s`)
+    startTime = endTime;
+
     core.info('Create bolt egress_rules.yaml...')
     fs.writeFileSync('egress_rules.yaml', egress_rules_yaml)
     await exec(`sudo cp egress_rules.yaml /home/${boltUser}/`)
     await exec(`sudo chown ${boltUser}:${boltUser} /home/${boltUser}/egress_rules.yaml`)
     core.info('Create bolt egress_rules.yaml... done')
 
+    endTime = Date.now();
+    core.info(`Time Elapsed in configuring egress_rules.yaml: ${Math.ceil((endTime - startTime)/1000)}s`)
+    startTime = endTime;
+
     core.info('Create intercept module...')
     await createInterceptDotPy(boltUser)
     await exec(`sudo cp intercept.py /home/${boltUser}/`)
     await exec(`sudo chown ${boltUser}:${boltUser} /home/${boltUser}/intercept.py`)
     core.info('Create intercept done...')
+
+    endTime = Date.now();
+    core.info(`Time Elapsed in creating intercept module: ${Math.ceil((endTime - startTime)/1000)}s`)
+    startTime = endTime;
     
     core.info('Create bolt service log files...')
     const logFile = `/home/${boltUser}/bolt.log`;
@@ -87,6 +117,10 @@ async function run() {
     await exec(`sudo chown ${boltUser}:${boltUser} ${logFile} ${errorLogFile}`)
     core.info('Create bolt service log files... done')
 
+    endTime = Date.now();
+    core.info(`Time Elapsed in creating Bolt service log files: ${Math.ceil((endTime - startTime)/1000)}s`)
+    startTime = endTime;
+
     core.info('Create bolt service...')
     const boltServiceConfig = await boltService(boltUser, mode, allow_http, default_policy, logFile, errorLogFile)
     fs.writeFileSync('bolt.service', boltServiceConfig)
@@ -94,6 +128,10 @@ async function run() {
     await exec('sudo chown root:root /etc/systemd/system/bolt.service')
     await exec('sudo systemctl daemon-reload')
     core.info('Create bolt service... done')
+
+    endTime = Date.now();
+    core.info(`Time Elapsed in creating Bolt Service: ${Math.ceil((endTime - startTime)/1000)}s`)
+    startTime = endTime;
     
     core.endGroup('setup-bolt')
     
@@ -107,6 +145,10 @@ async function run() {
     await wait(ms)
     await exec('sudo systemctl status bolt')
     core.info('Starting bolt... done')
+
+    endTime = Date.now();
+    core.info(`Time Elapsed in starting MITMProxy: ${Math.ceil((endTime - startTime)/1000)}s`)
+    startTime = endTime;
 
     core.endGroup('run-bolt')
 
@@ -128,6 +170,11 @@ async function run() {
       `sudo ip6tables -t nat -A OUTPUT -p tcp -m owner ! --uid-owner ${boltUser} --dport 443 -j REDIRECT --to-port 8080`
     )
     core.endGroup('setup-iptables-redirection')
+
+    endTime = Date.now();
+    core.info(`Time Elapsed in IPTableRedirection: ${Math.ceil((endTime - startTime)/1000)}s`)
+    startTime = endTime;
+
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
